a3250609e734a0199268d25c038d447a47550634
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-03-05 10:33:13 -0500

    Init commit after wp-cli scaffold command

---
+++ patched-up-email-formatter.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Patched Up Email Formatter
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: patched-up-email-formatter
+Domain Path: /languages
+*/
+
