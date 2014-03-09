03d7c3e8c7fd49c7fe74b546b6e8bcfe603a2919
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-03-09 17:23:07 +0000

    Adds functions for testing script and wp_mail_from for identification

--- class-patched-up-email-formatter.php
+++ class-patched-up-email-formatter.php
@@ -1,11 +1,19 @@
 <?php
 
 	/* With help from http://code.tutsplus.com/tutorials/creating-customized-comment-emails-building-a-plugin--wp-28681
+  * http://www.artishock.net/wordpress/how-to-change-wordpress-default-email-from-name-and-from-address/
 	*/
 
 	class Patched_Up_Email_Formatter {
 
 		function __construct() {
+      add_filter( 'wp_mail_from', function($email) {
+        return 'caseypatrickdriscoll@gmail.com';
+      });
+      add_filter( 'wp_mail_from_name', function($name) {
+        return 'Casey Patrick Driscoll';
+      });
+  
 			// Moderation
 			add_filter( 'comment_moderation_headers', array( $this, 'email_headers' ) );
 			add_filter( 'comment_moderation_subject', array( $this, 'email_subject' ), 10, 2 );
@@ -35,27 +43,66 @@
 
 			$comment = get_comment( $comment_id );
 
+      $approve_nonce = esc_html( '_wpnonce=' . wp_create_nonce( "approve-comment_$comment_id" ) );
+      $approval_url = home_url( '/' ) . 
+                      "wp-admin/comment.php?c=$comment->comment_ID" . 
+                      "&action=approvecomment&$approve_nonce";
+
+      $script =					
+        '{
+          "@context": "http://schema.org",
+          "@type": "EmailMessage", 
+          "action": {
+            "@type": "ConfirmAction",
+            "name": "Approve Comment",
+            "handler": {
+              "@type": "HttpActionHandler",
+              "url": "' . $approval_url . '"
+            }
+          },
+          "description": "Approval request for SOME COMMENT" 
+        }'; 
+
+      $script = 
+        '{
+          "@context":              "http://schema.org",
+          "@type":                 "EventReservation",
+          "reservationNumber":     "IO12345",
+          "underName": {
+            "@type":               "Person",
+            "name":                "John Smith"
+          },
+          "reservationFor": {
+            "@type":               "Event",
+            "name":                "Google I/O 2014",
+            "startDate":           "2014-05-15T08:30:00-08:00",
+            "location": {
+              "@type":             "Place",
+              "name":              "Moscone Center",
+              "address": {
+                "@type":           "PostalAddress",
+                "streetAddress":   "800 Howard St.",
+                "addressLocality": "San Francisco",
+                "addressRegion":   "CA",
+                "postalCode":      "94103",
+                "addressCountry":  "US"
+              }
+            }
+          }
+        }';
+
 			$body = '<html>
 								<body>
-									<script type="application/ld+json">
-										{
-											"@context": "http://schema.org",
-											"@type": "EmailMessage", 
-											"action": {
-												"@type": "ConfirmAction",
-												"name": "Approve Comment",
-												"handler": {
-													"@type": "HttpActionHandler",
-													"url": "https://myexpenses.com/approve?expenseId=abc123"
-												}
-											},
-											"description": "Approval request for John\'s $10.13 expense for office supplies"
-										} 
-									</script>
+									<script type="application/ld+json">' .
+                    $script . '
+                  </script>
 									<h1>New comment</h1>' .
 									"<p>hi you have a new message: $comment->comment_content</p>" . 
+                  "<a href='$approval_url'>$approval_url</a>" .
+                  "<pre>$script</pre>" . 
 								'</body>
 							</html>';
+
 
 			return $body;
 
--- patched-up-email-formatter.php
+++ patched-up-email-formatter.php
@@ -12,6 +12,23 @@ Domain Path: /languages
 
 include_once 'class-patched-up-email-formatter.php';
 
+/*
+Plugin Name: Email Return Path Fix
+Author: Abdussamad Abdurrazzaq
+Plugin URI: http://abdussamad.com/archives/567-Fixing-the-WordPress-Email-Return-Path-Header.html
+*/
+class email_return_path {
+    function __construct() {
+    add_action( 'phpmailer_init', array( $this, 'fix' ) );    
+    }
+ 
+  function fix( $phpmailer ) {
+      $phpmailer->Sender = $phpmailer->From;
+  }
+}
+ 
+new email_return_path();
+
 new Patched_Up_Email_Formatter();
 
 ?>
