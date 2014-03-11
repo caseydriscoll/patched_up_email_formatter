a5dea2ec3a1b4d6cbee6ed403bb366402345690a
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-03-11 15:56:20 +0000

    Refactors a bit for easier reading

--- patched-up-email-formatter.php
+++ patched-up-email-formatter.php
@@ -1,33 +1,22 @@
 <?php
 /*
 Plugin Name: Patched Up Email Formatter
-Version: 0.1.1
+Version: 0.1.2
 Description: A quick email formatter
 Author: Casey Patrick Driscoll
 Author URI: http://caseypatrickdriscoll.com
-Plugin URI: 
 Text Domain: patched-up-email-formatter
 Domain Path: /languages
 */
 
 include_once 'class-patched-up-email-formatter.php';
 
-/*
-Plugin Name: Email Return Path Fix
-Author: Abdussamad Abdurrazzaq
-Plugin URI: http://abdussamad.com/archives/567-Fixing-the-WordPress-Email-Return-Path-Header.html
-*/
-class email_return_path {
-    function __construct() {
-    add_action( 'phpmailer_init', array( $this, 'fix' ) );    
-    }
+// Example taken from: http://abdussamad.com/archives/567-Fixing-the-WordPress-Email-Return-Path-Header.html
+// Must correct email headers so Gmail with render buttons (only from authentic senders)
+add_action( 'phpmailer_init', array( $this, function($phpmailer) { 
+  $phpmailer->Sender = $phpmailer->From; 
+}));
  
-  function fix( $phpmailer ) {
-      $phpmailer->Sender = $phpmailer->From;
-  }
-}
- 
-new email_return_path();
 
 new Patched_Up_Email_Formatter();
 
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: caseypatrickdriscoll
 Tags: email
 Requires at least: 3.8.1
 Tested up to: 3.8.1
-Version: 0.1.1
+Version: 0.1.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
