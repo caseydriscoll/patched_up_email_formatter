04676ace5ce4af71c258721bf220e648babfe1fd
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-03-11 17:11:39 +0000

    Fixes version typo

--- patched-up-email-formatter.php
+++ patched-up-email-formatter.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Patched Up Email Formatter
-Version: 0.2.2
+Version: 0.2.1
 Description: A quick email formatter
 Author: Casey Patrick Driscoll
 Author URI: http://caseypatrickdriscoll.com
